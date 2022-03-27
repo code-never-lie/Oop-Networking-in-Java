fbf51ae007359b8211729413a9f9b2b6fe3a27ef
Ahmad Raza <79039768+code-never-lie@users.noreply.github.com>
2022-03-27 14:32:08 +0500

    Add files via upload

---
+++ 25 Networking.java
@@ -0,0 +1,105 @@
+Network Programming
+
+What is Network?
+
+A certain number of Autonomous, dispersed but interconnected computer via  
+Communication path in order to share information.
+
+- Autonomous (Controlling itself)
+
+- Dispersed ( Geographically Dispersed in a room, Building or all over the 
+ 		 world)
+
+- Communication Path (cable oriented/ cable less)
+
+OSI Model
+- Physical Layer
+- Data Link Layer
+- Network Layer
+- Transport Layer
+- Session Layer
+- Presentation Layer
+- Application Layer
+
+Physical Layer
+- Relationship between devices and physical Medium
+- Mutiplexing Vs Demultiplexing
+- Modulation and Demodulation
+Devices
+ - Repeater
+ - Mutiplexer/Demultiplexer
+
+Data Link Layer
+- Error and Flow Control
+- MAC(Media Access Control) vs LLC (Logical Link Control)IEEE802.2
+- MAC Address (6 bytes)
+- Access Methods (CSMACD,CSMACA)
+- Topologies logical(Bus, Ring), Physical(star,bus,mesh etc)
+- Ethernet (100BT,100BFx,100B2) IEEE802.3 wired IEEE802.11 Wireless
+- Channel Properties(Atteneuation, bandwidth, noise )
+Devices
+- Bridges
+- Lan Card
+
+Network Layer
+-include routing protocols, multicast group management, network-layer information and error, and network-layer address assignment.
+- Logical Addressing Scheme (Ip4 vs Ipv6) 
+- Assembling and Deassebling Packets
+- Packet Switches
+- Network Routes
+- Routing Table
+devices
+ - Router
+ -Brouter
+
+Transport Layer
+- transparent transfer of data between end users, providing reliable data     transfer services to the upper layers.
+-The transport layer controls the reliability of a given link through flow   control, segmentation/desegmentation, and error contro
+- Domain Names
+- Connection Oriented vs Connection Less Protocols
+- TCP vs UDP
+- TCP (Transmission Control Protocol) is a connection-based protocol that provides a reliable flow of data between two computers.
+
+-UDP (User Datagram Protocol) is a protocol that sends independent packets of data, called datagrams, from one computer to another with no guarantees about arrival. UDP is not connection-based like TCP.
+
+Devices
+ - Router
+ - Brouter
+
+Session Layer
+-  It establishes, manages and terminates the connections between the local   and remote application
+- 3 way handShaking
+- It provides for full-duplex, half-duplex, or simplex operation
+- Ports
+- The TCP and UDP protocols use ports to map incoming data to a particular   process running on a computer.
+- Connection Establishment and Release
+
+Presentation Layer
+- Presents data according to network and format
+- Marshaling and Unmarshalling
+- Data Independence
+
+Application Layer
+- This layer interacts with software applications that implement a    communicating component.
+- network application Sofwares
+Examples
+- Hypertext Transfer Protocol (HTTP),
+- File Transfer Protocol (FTP),
+- Simple Mail Transfer Protocol (SMTP)
+- Simple Network Management Protocol (SNMP).
+
+
+Network Programming in Java
+- java Support Network Programming through java.sql package
+
+Client/Server Programming
+
+
+
+
+
+
+
+
+
+
